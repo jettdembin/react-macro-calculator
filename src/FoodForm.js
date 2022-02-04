b1db141ef9c3ff777b570647f69a8ff036e8cd4b
Jett <premiumconsulting@jettdembin.dev>
2022-02-04 11:14:54 -0500

    changed food form to hopefully have working showing of remaining macros on food added, first commit after rebase attempt from detached head state

--- src/FoodForm.js
+++ src/FoodForm.js
@@ -15,9 +15,9 @@ function FoodForm(props) {
         onSubmit={(e) => {
           e.preventDefault();
           props.addFood(value, carb, protein, fat);
-          // props.handleCarbAdded(props.remainingCarb, carb);
-          // props.handleProteinAdded(props.remainingProtein, protein);
-          // props.handleFatAdded(props.remainingFat, fat);
+          props.handleCombinedCarb(props.totalCombinedCarb, carb);
+          props.handleCombinedProtein(props.totalCombinedProtein, protein);
+          props.handleCombinedFat(props.totalCombinedFat, fat);
           reset();
           resetCarb();
           resetProtein();
